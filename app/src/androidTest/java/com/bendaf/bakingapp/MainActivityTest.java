bdf8f3eb37ae5bc07a740c76c836d0902c910bd3
bendaf <bendaf92@gmail.com>
2018-08-21 00:41:00 +0200

    Add espresso tests.

---
+++ app/src/androidTest/java/com/bendaf/bakingapp/MainActivityTest.java
@@ -0,0 +1,65 @@
+package com.bendaf.bakingapp;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActTestRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = getTargetContext();
+
+        assertEquals("com.bendaf.bakingapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void clickOnItems(){
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.rec_recipes))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        intended(hasComponent(new ComponentName(getTargetContext(), StepListActivity.class)));
+
+    }
+
+    @Test
+    public void displayItems(){
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.rec_recipes)).check(new RecyclerViewItemCountAssertion(4));
+    }
+
+}
---
+++ app/src/androidTest/java/com/bendaf/bakingapp/RecyclerViewItemCountAssertion.java
@@ -0,0 +1,33 @@
+package com.bendaf.bakingapp;
+
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAssertion;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * Created by bendaf on 2018. 08. 21. BakingApp.
+ */
+
+public class RecyclerViewItemCountAssertion implements ViewAssertion {
+    private final int expectedCount;
+
+    RecyclerViewItemCountAssertion(int expectedCount) {
+        this.expectedCount = expectedCount;
+    }
+
+    @Override
+    public void check(View view, NoMatchingViewException noViewFoundException) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException;
+        }
+
+        RecyclerView recyclerView = (RecyclerView) view;
+        RecyclerView.Adapter adapter = recyclerView.getAdapter();
+        assert adapter != null;
+        assertThat(adapter.getItemCount(), is(expectedCount));
+    }
+}
