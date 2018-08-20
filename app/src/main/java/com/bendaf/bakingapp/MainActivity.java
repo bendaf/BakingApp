d0678108dfbe36cbea47cca2f5fda0a8664e9c59
bendaf <bendaf92@gmail.com>
2018-08-20 15:14:29 +0200

    Add tablet version of MainActivity

--- app/src/main/java/com/bendaf/bakingapp/MainActivity.java
+++ app/src/main/java/com/bendaf/bakingapp/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -66,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mBinding.recRecipes.setLayoutManager(new LinearLayoutManager(this));
         mAdapter = new RecipeAdapter(mRecipes);
         mBinding.recRecipes.setAdapter(mAdapter);
     }
